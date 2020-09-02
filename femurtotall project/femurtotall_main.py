76d6cfc27afcfa53db0951f17078508772077f41
ChanHyeok <wnrnrxla@gmail.com>
2020-09-03 00:14:49 +0900

    Test: vscode test

--- femurtotall project/femurtotall_main.py
+++ femurtotall project/femurtotall_main.py
@@ -5,6 +5,28 @@ import femurtotall
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
 
+#Male
+def M_pearson(number):
+    result = 81.306 + 1.880 * number
+    return result
+
+def M_Trotter(number):
+    result = 2.15 * number + 72.57
+    return result
+
+def M_huzii(number):
+    result = (2.47 * (number*10) + 549.01)/10
+    return result
+
+#Female
+def F_pearson(number):
+    result = 72.844 + 1.945 * number
+    return result
+
+def F_huzii(number):
+    result = (2.24 * (number*10) + 610.43)/10
+    return result
+
 wb = Workbook()
 ws = wb.active
 ws.title = 'result'
